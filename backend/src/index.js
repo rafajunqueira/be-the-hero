776adde9ff388bca924c3c43574e7e80db8b5aad
rafajunqueira <rafa.rjunqueira@gmail.com>
2020-03-29 13:59:49 -0300

    backend-done-frontend-half-done

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+/**
+ * Rotas / Recursos
+ * 
+ * MÉTODOS HTTP:
+ * GET          BUSCAR / LISTAR UMA INFORMAÇÃO NO BACK END
+ * POST         CRIAR UMA INFORMAÇÃO NO BACK END
+ * PUT          ALTERAR UMA INFORMAÇÃO NO BACK END       
+ * DELETE       DELETAR UMA INFORMAÇÃO NO BACK END
+ */
+
+/** 
+ * TIPOS DE PARÂMETROS
+ * Query Params: PARÂMETROS NOMEADOS ENVIADOS NA ROTA APÓS O SÍMBOLO DE ? (FILTROS, PAGINAÇÃO)
+ * Route Params: PARÂMETROS UTILIZADOS PARA IDENTIFICAR RECURSOS
+ * Request Body: CORPO DA REQUISIÇÃO UTILIZADO P/ CRIAR/ALTERAR RECURSOS
+ */
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
